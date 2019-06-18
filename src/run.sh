b1a50d3dab1ecfbfe3dec819ccd105317cefedea
ivelascog1 <i.velascog@alumnos.urjc.es>
2019-06-18 11:18:35 +0200

    Added new scripts to MeshReconstruct

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 source "$1/bin/activate"
-python3 compute_areas.py -a test.csv -v test.vrml -s None
-source "$1/bin/deacticate"
+shift
+python3 src/compute_areas.py $*
+deactivate
